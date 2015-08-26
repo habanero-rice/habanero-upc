30cd88b4d6e530e0e0ae50f935c083a1e4675e19
Vivek Kumar <vivek@staff-84-dun20-089.rice.edu>
2015-08-26 13:36:20 -0500

    readme

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
+########################################
+##### SET FOLLOWING PATHS CORRECTLY ####
+########################################
+
+# ABSOLUTE PATH TO UPC++ INSTALLATION DIRECTORY
 export UPCPP_ROOT=/Users/vivek/rice_work/projects/upcxx-repos/upcxx-install
+
+# ABSOLUTE PATH TO HABANERO-C++ BASE DIRECTORY
 export HCPP_BASE=/Users/vivek/rice_work/projects/release/hcpp
+
+# MPI C++ COMPILER
 export CXX=mpicxx
+
+# C COMPILER
 export CC=gcc
+
+# ABSOLUTE PATH TO LIBXML2 INSTALLATION
 export LIBXML2=/usr/local/Cellar/libxml2/2.9.2
+
+# ABSOLUTE PATH TO HABANERO-UPC BASE DIRECTORY
+export BASE=/Users/vivek/rice_work/projects/release/habanero-upc
+
 #export TBB_MALLOC=/Users/vivek/rice_work/projects/tbb/mac
 
 ################################################
@@ -13,10 +30,6 @@ export LIBXML2=/usr/local/Cellar/libxml2/2.9.2
 # CHANGING THE INSTALLATION PATH OF HABANERO-UPC
 #
 ################################################
-
-cd ..
-BASE=`pwd`
-cd -
 
 export HCPP_ROOT=${HCPP_BASE}/hcpp-install
 export HABANERO_UPC_ROOT=${BASE}/habanero-upc-install
