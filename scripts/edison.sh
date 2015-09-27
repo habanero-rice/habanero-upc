5d22a2c4773ba06b25043966da0b5e3edb09a86b
Vivek Kumar <vivek@vivek-2.local>
2015-09-27 14:15:06 -0500

    update edison script

--- scripts/edison.sh
+++ scripts/edison.sh
@@ -20,12 +20,14 @@ setenv HCPP_BASE /global/u2/v/vivekk/pgas14/github/hcpp
 #
 ################################################
 
+setenv LIBXML2_INCLUDE /usr/include/libxml2
+setenv LIBXML2_LIBS /usr/lib
 setenv CXX "CC -DGASNETT_USE_GCC_ATTRIBUTE_MAYALIAS=1"
 setenv CXX_LINKER g++
 setenv CC cc
 setenv HCPP_ROOT ${HCPP_BASE}/hcpp-install
 setenv HABANERO_UPC_ROOT ${BASE}/habanero-upc-install
-setenv LD_LIBRARY_PATH ${HABANERO_UPC_ROOT}/lib:${HCPP_ROOT}/lib:$LD_LIBRARY_PATH
+setenv LD_LIBRARY_PATH ${HABANERO_UPC_ROOT}/lib:${HCPP_ROOT}/lib:${LIBXML2_LIBS}:$LD_LIBRARY_PATH
 
 #if [ ! -z "${TBB_MALLOC}" ]; then
 #   setenv LD_LIBRARY_PATH ${TBB_MALLOC}:$LD_LIBRARY_PATH
