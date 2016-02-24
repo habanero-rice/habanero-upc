18780d925d747d3137930e8d3a84a39db270da30
Vivek Kumar <vivekk@rice.edu>
2016-02-24 12:49:53 -0800

    make nqueens parameters to be same as used in papers

--- test/distWS/nqueens/nqueens.cpp
+++ test/distWS/nqueens/nqueens.cpp
@@ -4,8 +4,8 @@
 #include "hcupc_spmd.h"
 
 #define LONG long
-#define SIZE 12
-static int threshold = SIZE-1;
+#define SIZE 18
+static int threshold = 7;
 
 LONG solutions[20] =
 {
@@ -25,8 +25,8 @@ LONG solutions[20] =
 		365596,
 		2279184,
 		14772512,
-		95815104,  /* N=17, mcut=7*/
-		666090624, /* N=18, mcut=7*/
+		95815104,
+		666090624, /* N=18 */
 		4968057848,
 		39029188884
 };
@@ -137,7 +137,7 @@ int main(int argc, char* argv[])
 		long end = get_usecs();
 		dur = ((double)(end-start))/1000000;
 		verify_queens(result);
-		printf("NQueens(%d) Time = %fsec\n",SIZE,dur);
+		printf("NQueens(%d)(threshold=%d) Time = %fsec\n",SIZE,threshold,dur);
 	}
 	delete(local_solutions);
 	hupcpp::finalize();
